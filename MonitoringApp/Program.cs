7c77ac09f26ee2aa441f226a55e82e6ac5dc3b61
Kroos <Kroos@DESKTOP-QHKKA3F>
2022-07-16 22:17:15 +0500

    Add project files.

---
+++ MonitoringApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using MonitoringLib;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace MonitoringApp
+{
+    class Program
+    {
+        /// <summary>
+        ///     Monitoring performance and memory use
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            /*Console.WriteLine("Processing. Please wait...");
+            Recorder.Start();
+            // simulate a process that requires some memory resour
+            int[] largeArrayOfInts =
+            Enumerable.Range(1, 10_000).ToArray();
+            // ...and takes some time to complete
+            Thread.Sleep(new Random().Next(5, 10) * 1000);
+            Recorder.Stop();
+            Console.ReadKey();*/
+
+            /*Measuring the efficiency of processing strings*/
+
+            int[] numbers = Enumerable.Range(1, 50_000).ToArray();
+            Console.WriteLine("Using string with +");
+            Recorder.Start();
+            string s = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                s += numbers[i] + ", ";
+            }
+            Recorder.Stop();
+            Console.WriteLine("Using StringBuilder");
+            Recorder.Start();
+            var builder = new System.Text.StringBuilder();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                builder.Append(numbers[i]); builder.Append(", ");
+            }
+            Recorder.Stop();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MonitoringLib/Recorder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Diagnostics.Process;
+
+namespace MonitoringLib
+{
+    public static class Recorder
+    {
+        static Stopwatch stopwatch = new Stopwatch();
+        static long bytesPhysicalBefore = 0;
+        static long bytesVirtualBefore = 0;
+        public static void Start()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            bytesPhysicalBefore = GetCurrentProcess().WorkingSet64;
+            bytesVirtualBefore = GetCurrentProcess().VirtualMemorySize64;
+            stopwatch.Restart();
+        }
+
+        public static void Stop()
+        {
+            stopwatch.Stop();
+            long bytesPhysicalAfter = GetCurrentProcess().WorkingSet64;
+            long bytesVirtualAfter = GetCurrentProcess().VirtualMemorySize64;
+
+            Console.WriteLine("{0:N0} physical bytes used.",bytesPhysicalAfter - bytesPhysicalBefore);
+            Console.WriteLine("{0:N0} virtual bytes used.",bytesVirtualAfter - bytesVirtualBefore);
+
+            Console.WriteLine("{0} time span ellapsed.", stopwatch.Elapsed);
+            Console.WriteLine("{0:N0} total milliseconds ellapsed.",stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
